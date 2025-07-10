1b548f03845f36021826bbf6377b85f58db2e19e
Facundo Busto <Facundo.Busto.I@gmail.com>
2025-07-10 19:43:51 -0300

    Add explanation to CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,4 @@
 
 load (Rails.root.join('db/seeds/users.rb'))
 load (Rails.root.join('db/seeds/courses.rb'))
-load (Rails.root.join('db/seeds/units.rb'))
-load (Rails.root.join('db/seeds/topic.rb'))
+load (Rails.root.join('db/seeds/ai_topic.rb'))
